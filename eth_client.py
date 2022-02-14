0c9e002646cf2cc6b30b1ea14c8a13c45e85345f
Seo <cv5006@naver.com>
2022-02-14 08:03:03 +0000

    Add exceptions

--- eth_client.py
+++ eth_client.py
@@ -8,10 +8,19 @@ SRV_ADDR = (SRV_IP, SRV_PORT)
 BUFF_SIZE = 1024
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cli:
-    cli.connect(SRV_ADDR)
+    try:
+        cli.connect(SRV_ADDR)
+    except ConnectionRefusedError:
+        print('Cannot connect')
+        exit()
+       
     while True:
         try:
             cli.send(str(input("input: ")).encode())
         except KeyboardInterrupt:
-            print('Client stop')
+            print('Stop client')
             break
+        except BrokenPipeError:
+            print('Server might be down')
+            break
+
