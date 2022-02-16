f16d541a063b5498ae47ec523ca28b723f0338c5
Seo <cv5006@naver.com>
2022-02-16 01:51:24 +0000

    Print response messages

--- eth_client.py
+++ eth_client.py
@@ -5,7 +5,16 @@ SRV_IP = '127.0.0.1'
 SRV_PORT = 8765
 SRV_ADDR = (SRV_IP, SRV_PORT)
 
-BUFF_SIZE = 1024
+reqs = {0:'Get', 1:'Set'}
+devs = {0:'ETH', 1:'UART'}
+dats = {0:'State', 1:'Routines', 2:'Period'}
+
+def DictString(dict:dict):
+    out_str = ''
+    for k, v in dict.items():
+        out_str += f'{str(k)}: {v}, '
+
+    return out_str
 
 def ByteSize(data):
     if data == 0:
@@ -21,12 +30,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cli:
         print('Cannot connect')
         exit()
        
+    i = 0
     while True:
-        try:            
-            req = int(input('Enter the Request\t 0: Get, 1: Set \n>> '))
-            dev = int(input('Enter the Device ID\t 0: ETH, 1: UART \n>> '))
-            dat = int(input('Enter the Data ID\t 0: State, 1: Routine, 2: Period \n>> '))
-            arg = int(input('Enter the Argument\t \n>> '))
+        i += 1
+        print(f'------------ SendRecv #{i} ------------')
+        try:
+            req = int(input(f'Enter the Request\t {DictString(reqs)} \n>> '))
+            dev = int(input(f'Enter the Device ID\t {DictString(devs)} \n>> '))
+            dat = int(input(f'Enter the Data ID\t {DictString(dats)} \n>> '))
+            if reqs[req] == 'Set':
+                arg = int(input(f'Enter the Argument\t \n>> '))
+            else:
+                arg = 0
 
             cmd_list = [req, dev, dat, arg]
             cmd_byte = bytes()
@@ -37,17 +52,28 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cli:
             cmd_byte = n_data.to_bytes(ByteSize(n_data), 'little') + cmd_byte
         except ValueError:
             print('Invalid format')
+            print()
             continue
         except KeyboardInterrupt:
             print('\nStop client')
             break
 
         try:
+            print()
             print(f'Send Req: {cmd_list}')
+
             cli.send(cmd_byte)
             n_data = int.from_bytes(cli.recv(1), 'little')
             res_list = list(cli.recv(n_data+1))
+
             print(f'Recv Res: {res_list}')
+            print()
+
+            result = 'Success' if (res_list[0] == 0) else 'Failed'
+            print(f'Request   Result : {result}')
+            print(f'Responsed Device : {devs[res_list[1]]}')
+            print(f'Responsed Data ID: {dats[res_list[2]]}')
+            print(f'Returned  Data   : {res_list[3:]}')
             print()
         except BrokenPipeError:
             print('Server might be down')
